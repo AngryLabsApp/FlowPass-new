8f9f02d560d573a40c20ed25ead4f335a7dd0b88
Grecia Delgado <greciadm2596@gmail.com>
2025-09-26 00:02:14 -0500

    update files

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,6 +6,12 @@
 
 <svelte:head>
 	<link rel="icon" href={favicon} />
+	<nav class="sidebar">
+  <a href="/alumnos" class="sidebar__link">Alumnos</a>
+  <a href="/ingresos" class="sidebar__link">Ingresos</a>
+  <a href="/pagos"   class="sidebar__link">Pagos</a>
+</nav>
+
 </svelte:head>
 
 {@render children?.()}
---
+++ src/routes/alumnos/page.svelte
@@ -0,0 +1,4 @@
+
+<!-- src/routes/alumnos/+page.svelte -->
+<h1>Hola Mundo 👋 desde Alumnos</h1>
+<p>Esta es tu primera página en FlowPass con SvelteKit.</p>
