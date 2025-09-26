ad95a6e53859af5b84856d73a28e46482e5167f7
Grecia Delgado <greciadm2596@gmail.com>
2025-09-26 16:38:40 -0500

    update

--- src/routes/alumnos/+page.svelte
+++ src/routes/alumnos/+page.svelte
@@ -1,4 +1,9 @@
+<script>
+  // @ts-ignore
+  import { PUBLIC_API_BASE_URL } from "$env/static/public";
+</script>
 
-<!-- src/routes/alumnos/+page.svelte -->
 <h1>Hola Mundo 👋 desde Alumnos</h1>
 <p>Esta es tu primera página en FlowPass con SvelteKit.</p>
+
+<p>API Base URL: {PUBLIC_API_BASE_URL}</p>
