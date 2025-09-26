e9265dd6318a6ab4f944dae5179a50e23ce5130e
Grecia Delgado <greciadm2596@gmail.com>
2025-09-26 18:15:58 -0500

    test: deploy

--- src/routes/alumnos/+page.svelte
+++ src/routes/alumnos/+page.svelte
@@ -3,7 +3,7 @@
   import { PUBLIC_API_BASE_URL } from "$env/static/public";
 </script>
 
-<h1>Hola Mundo 👋 desde Alumnos</h1>
+<h1>Hola Mundo 👋 desde Alumnos!!!!! porfin prueba  </h1>
 <p>Esta es tu primera página en FlowPass con SvelteKit.</p>
 
 <p>API Base URL: {PUBLIC_API_BASE_URL}</p>
