562133f9034622f0c8722127b52128dfb3ce45d1
Grecia Delgado <greciadm2596@gmail.com>
2025-10-01 13:35:09 -0500

    instala tailwind

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,12 @@
+
 <script lang="ts">
   import favicon from "$lib/assets/favicon.svg";
+  import "../lib/styles/app.css";
+
+
+  
 </script>
+
 
 <svelte:head>
   <link rel="icon" href={favicon} />
@@ -10,6 +16,9 @@
   <a href="/alumnos" class="sidebar__link">Alumnos</a>
   <a href="/ingresos" class="sidebar__link">Ingresos</a>
   <a href="/pagos" class="sidebar__link">Pagos</a>
+   <h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
 </nav>
 
 <main class="content">
