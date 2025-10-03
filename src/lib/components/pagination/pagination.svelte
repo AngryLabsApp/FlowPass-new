fcf8a631e8fe01691da3a9675b4c5ad189a9ee77
Grecia Delgado <greciadm2596@gmail.com>
2025-10-03 02:31:16 -0500

    update paginacion

--- src/lib/components/pagination/pagination.svelte
+++ src/lib/components/pagination/pagination.svelte
@@ -8,9 +8,14 @@
     page = $bindable(1),
     onSearch,
   } = $props<{
-    pagination_values: { total: number; start: number; end: number; totalPages: number };
+    pagination_values: {
+      total: number;
+      start: number;
+      end: number;
+      totalPages: number;
+    };
     page: number;
-    onSearch?: (key: FilterKeys, q: string) => void; 
+    onSearch?: (key: FilterKeys, q: string) => void;
   }>();
 
   function handlePageChange(current_page: number) {
@@ -19,29 +24,40 @@
   }
 </script>
 
-
-<div class="flex flex-col items-center justify-center gap-3">
-  <div class="flex flex-col items-center justify-center gap-2">
-    <div class="text-sm text-gray-700 dark:text-gray-400">
-      Mostrando <span class="font-semibold text-gray-900 dark:text-white">{pagination_values.start}</span>
-      -
-      <span class="font-semibold text-gray-900 dark:text-white">{pagination_values.end}</span>
-      de
-      <span class="font-semibold text-gray-900 dark:text-white">{pagination_values.total}</span>
-      
-    </div>
-  <PaginationNav  visiblePages={5} currentPage={page} totalPages={pagination_values.totalPages} onPageChange={handlePageChange}>
-  {#snippet prevContent()}
-    <span class="sr-only">Previous</span>
-    <ArrowLeftOutline class="h-5 w-5" />
-  {/snippet}
-  {#snippet nextContent()}
-    <span class="sr-only">Next</span>
-    <ArrowRightOutline class="h-5 w-5" />
-  {/snippet}
-</PaginationNav>
+<div class="flex items-center justify-between gap-3">
+  <!-- Texto a la derecha -->
+  <div class="text-sm text-gray-700 dark:text-gray-400">
+    Mostrando
+    <span class="font-semibold text-gray-900 dark:text-white"
+      >{pagination_values.start}</span
+    >
+    -
+    <span class="font-semibold text-gray-900 dark:text-white"
+      >{pagination_values.end}</span
+    >
+    de
+    <span class="font-semibold text-gray-900 dark:text-white"
+      >{pagination_values.total}</span
+    >
   </div>
+  <!-- Paginación a la izquierda -->
+  <PaginationNav
+    visiblePages={10}
+    currentPage={page}
+    totalPages={pagination_values.totalPages}
+    onPageChange={handlePageChange}
+    layout="pagination"
+    classes={{
+      active: "bg-green-100 dark:bg-green-700 text-green-600 dark:text-white",
+    }}
+  >
+    {#snippet prevContent()}
+      <span class="sr-only">Previous</span>
+      <ArrowLeftOutline class="h-5 w-5" />
+    {/snippet}
+    {#snippet nextContent()}
+      <span class="sr-only">Next</span>
+      <ArrowRightOutline class="h-5 w-5" />
+    {/snippet}
+  </PaginationNav>
 </div>
-
-
- 
