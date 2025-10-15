90322ad4b0b46829e88ca5bb7ced840c7d656fa9
Grecia Delgado <greciadm2596@gmail.com>
2025-10-14 21:18:48 -0500

    stilos loading

--- src/lib/components/loader/loader.svelte
+++ src/lib/components/loader/loader.svelte
@@ -1,11 +1,28 @@
 <script lang="ts">
-  import { Modal, Spinner } from "flowbite-svelte";
-  let { openModal = $bindable(false), title  } = $props<{
+  import { Button, Modal, Spinner } from "flowbite-svelte";
+  let { openModal = $bindable(false), title } = $props<{
     openModal: boolean;
     title: string;
   }>();
 </script>
 
-<Modal form title={title} bind:open={openModal} size="xs" permanent={true} dismissable={true} outsideclose={false}>
-  <Spinner size="8" />
+<Modal
+  form
+  bind:open={openModal}
+  size="xs"
+  permanent={true}
+  dismissable={true}
+  outsideclose={false}
+  class=" text-center"
+>
+  <div class="flex flex-col items-center pb-4">
+    <div
+      class="mt-4 flex space-x-3 lg:mt-6 rtl:space-x-reverse text-center items-center"
+    >
+      <Spinner size="16" class="text-center" color="primary" />
+      <h5 class="mb-1 text-xl font-medium text-gray-900 dark:text-white">
+        {title}
+      </h5>
+    </div>
+  </div>
 </Modal>
