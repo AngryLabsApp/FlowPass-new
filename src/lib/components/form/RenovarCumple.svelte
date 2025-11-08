c6ba7529c58331947238f63b20ebef52cee79c3f
Grecia Delgado <greciadm2596@gmail.com>
2025-11-08 16:26:44 -0500

    Update format of component RenovarCumple

--- src/lib/components/form/RenovarCumple.svelte
+++ src/lib/components/form/RenovarCumple.svelte
@@ -1,21 +1,17 @@
 <script lang="ts">
-    import type { FormProps } from "$lib/catalog/form_component_catalog";
-    import { UserKeys } from "$lib/enums/user_keys";
-    import { useFormUpdateHook, type UpdateFormItem } from "$lib/hooks/useFormUpdate.svelte";
-    import { fmtDateToString } from "$lib/utils/utils";
+  import type { FormProps } from "$lib/catalog/form_component_catalog";
+  import { UserKeys } from "$lib/enums/user_keys";
   import {
-    Button,
-    Card,
-    Label,
-    Datepicker,
-  } from "flowbite-svelte";
+    useFormUpdateHook,
+    type UpdateFormItem,
+  } from "$lib/hooks/useFormUpdate.svelte";
+  import { fmtDateToString } from "$lib/utils/utils";
+  import { Button, Card, Label, Datepicker } from "flowbite-svelte";
 
-    let { user, setLoadingModal, setToast, closeForm }: FormProps = $props();
+  let { user, setLoadingModal, setToast, closeForm }: FormProps = $props();
 
   let selectedDate = $state<Date | undefined>(
-    user.cumpleanos
-      ? new Date(user.cumpleanos + "T00:00:00")
-      : undefined,
+    user.cumpleanos ? new Date(user.cumpleanos + "T00:00:00") : undefined
   );
   let updateItemValues: UpdateFormItem[] = $state([
     { key: UserKeys.CUMPLEANOS, value: user.cumpleanos || "" },
@@ -33,7 +29,10 @@
 </script>
 
 <Card class="p-4 sm:p-6 md:p-8">
- <form class="flex flex-col space-y-6" onsubmit={(e)=>actions.onUpdateSingleForm(updateItemValues, user.id, e)}>
+  <form
+    class="flex flex-col space-y-6"
+    onsubmit={(e) => actions.onUpdateSingleForm(updateItemValues, user.id, e)}
+  >
     <h3 class="text-xl font-medium text-gray-900 dark:text-white">
       Fecha de cumpleaños
     </h3>
@@ -46,9 +45,6 @@
         required
       />
     </Label>
-    <Button
-      type ="submit"
-      class="w-full">Guardar cambios</Button
-    >
+    <Button type="submit" class="w-full">Guardar cambios</Button>
   </form>
 </Card>
